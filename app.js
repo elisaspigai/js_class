bde5f48ec69702b67416d99149f73ae05e5e0999
elisaspigai <elisaspigai@gmail.com>
2015-10-14 11:10:18 +0300

    task 2

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log('hello')
+
+var food = ['pasta', 'chocolate', 'steak', 'pizza'];
+
+var favourite = prompt("What's your favourite food?");
+
+if(favourite = food)
+{
+	console.log("good taste!")
+}
+else{
+	cosole.log("too bad!")
+}
